3f7f91e064c24508966784163db465f37d9a23b6
OgataiKhan <badellino.kevin@gmail.com>
2023-11-10 16:09:40 +0100

    feat: cycle images on click

--- js/main.js
+++ js/main.js
@@ -7,30 +7,33 @@
 const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
 const items = document.querySelector('.items');
 let currentSlide = 0;
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
 
-// 4. We use a for loop to add the images to the html using a template literal.
+// 5. We use a for loop to add the images to the html using a template literal.
 for (let i = 0; i < images.length; i++) {
     items.innerHTML += `<div class="item"><img src="img/${images[i]}" alt="Image ${i+1}"></div>`;
 }
 
-// 5. We add the "active" class to the first slide.
+// // 6. We add the "active" class to the first slide.
 const firstItem = items.querySelector('.item');
 firstItem.classList.add('active');
 
-// 6. We create a variable that selects all of our slides in the html.
+// // 7. We create a variable that selects all of our slides in the html.
 const domSlides = document.querySelectorAll('.item');
 
-// 7. We use addEventListener to cycle through the images when we click on the arrows, making sure we can't continue past the first and last image.
+// 8. We use addEventListener to cycle through the images when we click on the arrows, making sure we can't continue past the first and last image.
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
 prev.addEventListener('click', function() {
-console.log('Ho cliccato su prev');
+    console.log('Ho cliccato su prev');
     if (currentSlide > 0) {
         domSlides[currentSlide].classList.remove('active');
         currentSlide--;
         domSlides[currentSlide].classList.add('active');   
     }
 })
+
 
 next.addEventListener('click', function() {
     console.log('Ho cliccato su next');
