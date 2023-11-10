87fbb78664d60db7344b599b07362765d4a938ff
OgataiKhan <badellino.kevin@gmail.com>
2023-11-10 17:36:20 +0100

    feat: add thumbnails that change on click

--- js/main.js
+++ js/main.js
@@ -7,18 +7,27 @@
 const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
 const items = document.querySelector('.items');
 let currentSlide = 0;
+// Bonus 2: We create a "thumbnails" variable that will contain html div element that will contain all our thumbnails.
+const thumbnails = document.querySelector('.thumbnails');
 
 // 5. We use a for loop to add the images to the html using a template literal.
+// Bonus 2: We also add the thumbnails.
 for (let i = 0; i < images.length; i++) {
     items.innerHTML += `<div class="item"><img src="img/${images[i]}" alt="Image ${i+1}"></div>`;
+    thumbnails.innerHTML += `<div class="thumbnail"><img src="img/${images[i]}" alt="Image ${i+1} thumbnail"></div>`
 }
 
-// // 6. We add the "active" class to the first slide.
+// 6. We add the "active" class to the first slide.
+// Bonus 2: We also add the "highlighted" class to the first thumbnail
 const firstItem = items.querySelector('.item');
 firstItem.classList.add('active');
+const firstThumbnail = thumbnails.querySelector('.thumbnail');
+firstThumbnail.classList.add('highlighted');
 
-// // 7. We create a variable that selects all of our slides in the html.
+// 7. We create a variable that selects all of our slides in the html.
+// Bonus 2: We also create a variable that selects all of our thumbnails in the html.
 const domSlides = document.querySelectorAll('.item');
+const domThumbnails = document.querySelectorAll('.thumbnail');
 
 // 8. We use addEventListener to cycle through the images when we click on the arrows, making sure we can't continue past the first and last image.
 // Bonus 1: add "else" conditions to make the carousel cyclical
@@ -28,23 +37,31 @@ const next = document.querySelector('.next');
 prev.addEventListener('click', function() {
     if (currentSlide > 0) {
         domSlides[currentSlide].classList.remove('active');
+        domThumbnails[currentSlide].classList.remove('highlighted');
         currentSlide--;
         domSlides[currentSlide].classList.add('active');
+        domThumbnails[currentSlide].classList.add('highlighted');
     } else {
         domSlides[currentSlide].classList.remove('active');
+        domThumbnails[currentSlide].classList.remove('highlighted');
         currentSlide = domSlides.length - 1;
         domSlides[currentSlide].classList.add('active');
+        domThumbnails[currentSlide].classList.add('highlighted');
     }
 })
 
 next.addEventListener('click', function() {
     if (currentSlide < domSlides.length - 1) {
         domSlides[currentSlide].classList.remove('active');
+        domThumbnails[currentSlide].classList.remove('highlighted');
         currentSlide++;
         domSlides[currentSlide].classList.add('active');
+        domThumbnails[currentSlide].classList.add('highlighted');
     } else {
         domSlides[currentSlide].classList.remove('active');
+        domThumbnails[currentSlide].classList.remove('highlighted');
         currentSlide = 0;
         domSlides[currentSlide].classList.add('active');
+        domThumbnails[currentSlide].classList.add('highlighted');
     }
 })
